fc4e3070b16b078ace1a53d9bc28b025c9e1a41f
bmisr <bmisr@DESKTOP-J3KHLGD.fios-router.home>
2018-10-14 11:58:52 -0400

    commit1

--- src/WhileDiceMain.java
+++ src/WhileDiceMain.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 
 public class WhileDiceMain {
 
 	public static void main(String[] args) 
 	{
+		Scanner in = new Scanner(System.in);
 		WhileDice dice1 = new WhileDice();
 		WhileDice dice2 = new WhileDice();
 		
@@ -18,6 +20,25 @@ public class WhileDiceMain {
 		}
 		System.out.println("Roll val:  " + roll1 + "\nSum of Rolls: " + (roll1 + roll2) + "\nNum rolls taken: " + numRolls);
 
+		int targetVal=0;
+		int counter = 0;
+		int sum=0;
+		while (targetVal < 2 || targetVal >12) 
+		{
+			System.out.print("Enter a number between 2 and 12: ");
+			targetVal = in.nextInt();
+			
+		}
+		while (sum != targetVal) 
+		{
+			int dice1Val = dice1.roll();
+			int dice2Val = dice2.roll();
+			sum = dice1Val + dice2Val;
+			++counter;
+			System.out.println(dice1Val + "+" + dice2Val + "=" + sum);
+		}
+		System.out.println("Rolls taken to get value: " + counter);
+		
 	}
 
 }
